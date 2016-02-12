ef729fdb2735459468cd658b1ac0c7993a691eb3
Brian Richardson <brisquatch@gmail.com>
2016-02-12 10:18:52 -0600

    create index page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Hayes Lawn &amp; Landscaping</title>
+</head>
+
+<body>
+	<blockquote>
+		<p>At Hayes Lawn &amp; Landscaping we strive to ensure that all of our customers are beyond satisfied at all times. I personally ask every client to please stay in touch and give us feedback on our service level.</p>
+
+		<p>I love working outside with the crew to make sure that our team is consistently doing the best work possible. It's been wonderful seeing our business grow over the years! God has been so good to me and my family and we are so thankful for our customers' support and allowing us to work with them.</p>
+	</blockquote>
+
+	&#8212;Austin Hayes, owner
+
+
+
+
+</body>
+
+</html>
